4d7b52b98f2882d9e0ef9507c372823329e62a60
FujiHaruka <fizzyjohn1427@gmail.com>
2022-03-14 20:15:16 +0900

    lib.rs for integ tests

---
+++ src/lib.rs
@@ -0,0 +1,30 @@
+mod json_util;
+mod notion_client;
+mod notion_database_schema;
+mod notion_list;
+mod sqlite;
+
+#[macro_use]
+extern crate log;
+
+use crate::notion_client::NotionClient;
+use crate::sqlite::Sqlite;
+use std::env;
+
+pub fn main() {
+    env_logger::init();
+
+    let api_key = env::var("NOTION_API_KEY").unwrap();
+    let database_id = env::var("NOTION_DATABASE_ID").unwrap();
+
+    let client = NotionClient { api_key };
+    let schema = client.get_database(&database_id).unwrap();
+    let list = client.get_all_entries(&database_id, &schema).unwrap();
+
+    let sqlite = Sqlite::new("notion.db", &schema).unwrap();
+    sqlite.create_table().unwrap();
+
+    for item in list {
+        sqlite.insert(&item).unwrap();
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,5 @@
-mod json_util;
-mod notion_client;
-mod notion_database_schema;
-mod notion_list;
-mod sqlite;
-
-#[macro_use]
-extern crate log;
-
-use crate::notion_client::NotionClient;
-use crate::sqlite::Sqlite;
-use std::env;
+extern crate notion_into_sqlite;
 
 fn main() {
-    env_logger::init();
-
-    let api_key = env::var("NOTION_API_KEY").unwrap();
-    let database_id = env::var("NOTION_DATABASE_ID").unwrap();
-
-    let client = NotionClient { api_key };
-    let schema = client.get_database(&database_id).unwrap();
-    let list = client.get_all_entries(&database_id, &schema).unwrap();
-
-    let sqlite = Sqlite::new("notion.db", &schema).unwrap();
-    sqlite.create_table().unwrap();
-
-    for item in list {
-        sqlite.insert(&item).unwrap();
-    }
+    notion_into_sqlite::main()
 }
