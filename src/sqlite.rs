3943f8ad6e0d861d910754a4668667f3fc6d8b7c
FujiHaruka <fizzyjohn1427@gmail.com>
2022-03-14 20:59:47 +0900

    moving tests

--- src/sqlite.rs
+++ src/sqlite.rs
@@ -5,9 +5,9 @@ use crate::{
 use rusqlite::{params_from_iter, Connection, Result};
 
 pub struct Sqlite<'a> {
-    conn: Connection,
-    table_name: String,
-    schema: &'a NotionDatabaseSchema,
+    pub conn: Connection,
+    pub table_name: String,
+    pub schema: &'a NotionDatabaseSchema,
 }
 impl Sqlite<'_> {
     pub fn new<'a>(path: &str, schema: &'a NotionDatabaseSchema) -> Result<Sqlite<'a>> {
@@ -94,106 +94,5 @@ impl Sqlite<'_> {
             columns_formatted.join(", "),
             placeholders.join(", ")
         )
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use serial_test::serial;
-    use std::collections::HashMap;
-    use std::fs;
-
-    use crate::notion_database::NotionProperty;
-
-    use super::*;
-
-    fn cleanup(database_path: &str) {
-        fs::remove_file(database_path).ok();
-    }
-
-    #[test]
-    #[serial]
-    fn test_create_table() {
-        let database_path = "tmp/test.db";
-        cleanup(database_path);
-
-        let schema = get_schema();
-        let sqlite = Sqlite::new(database_path, &schema).unwrap();
-        sqlite.create_table().unwrap();
-
-        let (table_name, sql): (String, String) = sqlite
-            .conn
-            .query_row("SELECT name, sql FROM sqlite_master", [], |row| {
-                Ok((row.get(0)?, row.get(1)?))
-            })
-            .unwrap();
-        assert_eq!(table_name, "notion");
-        assert!(sql.contains(r#""Name" TEXT"#));
-        assert!(sql.contains(r#""Animal" TEXT"#));
-        assert!(sql.contains(r#""Age" REAL"#));
-    }
-
-    #[test]
-    #[serial]
-    fn test_insert() {
-        let database_path = "tmp/test.db";
-        cleanup(database_path);
-
-        let schema = get_schema();
-        let sqlite = Sqlite::new(database_path, &schema).unwrap();
-        sqlite.create_table().unwrap();
-
-        let entry = NotionEntry {
-            id: "xxxx".to_string(),
-            properties: HashMap::from([
-                (
-                    "Name".to_string(),
-                    NotionPropertyValue::Text("Meu".to_string()),
-                ),
-                ("Age".to_string(), NotionPropertyValue::Number(5.0)),
-            ]),
-        };
-        sqlite.insert(&entry).unwrap();
-
-        let (id, name, age): (String, String, f64) = sqlite
-            .conn
-            .query_row(r#"SELECT id,"Name","Age" from notion"#, [], |row| {
-                Ok((row.get(0)?, row.get(1)?, row.get(2)?))
-            })
-            .unwrap();
-        assert_eq!(id, "xxxx");
-        assert_eq!(name, "Meu");
-        assert_eq!(age, 5.0);
-    }
-
-    fn get_schema() -> NotionDatabaseSchema {
-        NotionDatabaseSchema {
-            properties: HashMap::from([
-                (
-                    "Name".to_string(),
-                    NotionProperty {
-                        name: "Name".to_string(),
-                        property_raw_type: "title".to_string(),
-                        property_type: NotionPropertyType::Title,
-                    },
-                ),
-                (
-                    "Age".to_string(),
-                    NotionProperty {
-                        name: "Age".to_string(),
-                        property_raw_type: "number".to_string(),
-                        property_type: NotionPropertyType::Number,
-                    },
-                ),
-                (
-                    "Animal".to_string(),
-                    NotionProperty {
-                        name: "Animal".to_string(),
-                        property_raw_type: "select".to_string(),
-                        property_type: NotionPropertyType::Select,
-                    },
-                ),
-            ]),
-        }
     }
 }
---
+++ tests/sqlite_test.rs
@@ -0,0 +1,73 @@
+mod fixtures;
+
+extern crate notion_into_sqlite;
+
+use serial_test::serial;
+use std::collections::HashMap;
+use std::fs;
+
+use notion_into_sqlite::notion_database::parse_database_schema;
+use notion_into_sqlite::notion_list::{NotionEntry, NotionPropertyValue};
+use notion_into_sqlite::sqlite::Sqlite;
+use std::error::Error;
+
+static DATABASE_PATH: &str = "tmp/test.db";
+
+fn cleanup(database_path: &str) {
+    fs::remove_file(database_path).ok();
+}
+
+#[test]
+#[serial]
+fn it_creates_table() -> Result<(), Box<dyn Error>> {
+    cleanup(DATABASE_PATH);
+
+    let schema = parse_database_schema(fixtures::NOTION_DATABASE_JSON)?;
+    let sqlite = Sqlite::new(DATABASE_PATH, &schema)?;
+    sqlite.create_table()?;
+
+    let (table_name, sql): (String, String) =
+        sqlite
+            .conn
+            .query_row("SELECT name, sql FROM sqlite_master", [], |row| {
+                Ok((row.get(0)?, row.get(1)?))
+            })?;
+    assert_eq!(table_name, "notion");
+    assert!(sql.contains(r#""Name" TEXT"#));
+    assert!(sql.contains(r#""Animal" TEXT"#));
+    assert!(sql.contains(r#""Age" REAL"#));
+    Ok(())
+}
+
+#[test]
+#[serial]
+fn it_inserts_notion_entry() -> Result<(), Box<dyn Error>> {
+    cleanup(DATABASE_PATH);
+
+    let schema = parse_database_schema(fixtures::NOTION_DATABASE_JSON)?;
+    let sqlite = Sqlite::new(DATABASE_PATH, &schema)?;
+    sqlite.create_table()?;
+
+    let entry = NotionEntry {
+        id: "xxxx".to_string(),
+        properties: HashMap::from([
+            (
+                "Name".to_string(),
+                NotionPropertyValue::Text("Meu".to_string()),
+            ),
+            ("Age".to_string(), NotionPropertyValue::Number(5.0)),
+        ]),
+    };
+    sqlite.insert(&entry)?;
+
+    let (id, name, age): (String, String, f64) =
+        sqlite
+            .conn
+            .query_row(r#"SELECT id,"Name","Age" from notion"#, [], |row| {
+                Ok((row.get(0)?, row.get(1)?, row.get(2)?))
+            })?;
+    assert_eq!(id, "xxxx");
+    assert_eq!(name, "Meu");
+    assert_eq!(age, 5.0);
+    Ok(())
+}
