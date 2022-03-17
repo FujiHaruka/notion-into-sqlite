c74ee49aa2a9f40636225312ecca8595c6c83649
FujiHaruka <fizzyjohn1427@gmail.com>
2022-03-17 20:53:32 +0900

    add unit test

--- tests/sqlite_test.rs
+++ tests/sqlite_test.rs
@@ -2,6 +2,7 @@ mod common;
 
 extern crate notion_into_sqlite;
 
+use rusqlite::params;
 use serial_test::serial;
 use std::collections::HashMap;
 use std::fs;
@@ -9,7 +10,9 @@ use std::fs;
 use common::fixtures;
 use notion_into_sqlite::notion_database::parse_database_schema;
 use notion_into_sqlite::notion_pages::{NotionPage, NotionPropertyValue};
-use notion_into_sqlite::sqlite::{Sqlite, PAGE_PROPERTIES_TABLE};
+use notion_into_sqlite::sqlite::{
+    Sqlite, PAGE_ID_COLUMN, PAGE_METADATA_TABLE, PAGE_PROPERTIES_TABLE,
+};
 use std::error::Error;
 
 static DATABASE_PATH: &str = "tmp/test.db";
@@ -28,16 +31,26 @@ fn it_creates_tables() -> Result<(), Box<dyn Error>> {
     let sqlite = Sqlite::new(DATABASE_PATH, &schema)?;
     sqlite.create_tables()?;
 
-    let (table_name, sql): (String, String) =
-        sqlite
-            .conn
-            .query_row("SELECT name, sql FROM sqlite_master", [], |row| {
-                Ok((row.get(0)?, row.get(1)?))
-            })?;
-    assert_eq!(table_name, PAGE_PROPERTIES_TABLE);
-    assert!(sql.contains(r#""Name" TEXT"#));
-    assert!(sql.contains(r#""Animal" TEXT"#));
-    assert!(sql.contains(r#""Age" REAL"#));
+    let table_def_sql: String = sqlite.conn.query_row(
+        "SELECT sql FROM sqlite_master where name=?1",
+        params![PAGE_PROPERTIES_TABLE],
+        |row| Ok(row.get(0)?),
+    )?;
+    assert!(table_def_sql.contains(&format!(
+        "{id_column} TEXT PRIMARY KEY",
+        id_column = PAGE_ID_COLUMN
+    )));
+    assert!(table_def_sql.contains(r#""Name" TEXT"#));
+    assert!(table_def_sql.contains(r#""Animal" TEXT"#));
+    assert!(table_def_sql.contains(r#""Age" REAL"#));
+
+    let table_def_sql: String = sqlite.conn.query_row(
+        "SELECT sql FROM sqlite_master where name=?1",
+        params![PAGE_METADATA_TABLE],
+        |row| Ok(row.get(0)?),
+    )?;
+    assert!(table_def_sql.contains(r#"url TEXT"#));
+
     Ok(())
 }
 
@@ -95,5 +108,18 @@ fn it_inserts_notion_entry() -> Result<(), Box<dyn Error>> {
     assert_eq!(page_id, "xxxx");
     assert_eq!(name, "Meu");
     assert_eq!(age, 5.0);
+
+    let (page_id, url): (String, String) = sqlite.conn.query_row(
+        format!(
+            r#"SELECT id, url from {table_name}"#,
+            table_name = PAGE_METADATA_TABLE
+        )
+        .as_str(),
+        [],
+        |row| Ok((row.get(0)?, row.get(1)?)),
+    )?;
+    assert_eq!(page_id, "xxxx");
+    assert_eq!(url, "https://www.notion.so/xxxx");
+
     Ok(())
 }
